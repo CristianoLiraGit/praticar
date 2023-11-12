44dd7443aed5881443750ff7e990416817b533c3
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2023-11-12 10:30:39 -0300

    Create Rick and Morty API parte 2
    Criada a pasta referente à aula 2.

---
+++ rickandmorty/parte2/script.js
@@ -0,0 +1,22 @@
+const characterId = document.getElementById('characterId');
+const btnGo = document.getElementById('btn_go');
+const content = document.getElementById('content');
+const image = document.getElementById('img');
+
+const fetchApi = (value) => {
+    const result = fetch(`https://rickandmortyapi.com/api/character/${value}`)
+    .then((res)=>res.json())
+    .then((data)=>{
+        console.log(data.name);
+        return data;
+    }); // Link da base de consulta (api geral)
+
+    return result;
+}
+
+btnGo.addEventListener('click', async (event) =>{
+    event.preventDefault(); // Prevenir o padrão, que é o carregamento da página.
+    const result = await fetchApi(characterId.value);
+    content.textContent = `${JSON.stringify(result, undefined, 2)}`; // o parâmetro 2 é para dar um espaço.
+    image.src = `${result.image}`;
+});
